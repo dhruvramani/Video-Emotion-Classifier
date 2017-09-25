813a6d354cd8b53672ec01cc5b0f3d5bd2551f15
dhruvramani <dhruvramani98@gmail.com>
2017-09-25 23:00:27 +0530

    Minor Changes

--- model/src/embedding.py
+++ model/src/embedding.py
@@ -12,4 +12,4 @@ class Embedding(object):
         gray_scale = np.asarray(gray_scale, dtype='uint8')
         faceCascade = cv2.CascadeClassifier(os.path.join(self.config.cascade_path , self.config.cascade))
         faces = faceCascade.detectMultiScale(gray_scale, scaleFactor=1.08, minNeighbors=1)
-        return gray_scale[faces[1]: faces[1] + faces[3], faces[2]: faces[2] + faces[4]]
+        return gray_scale[faces[1]: faces[1] + faces[2], faces[0]: faces[0] + faces[3]]
