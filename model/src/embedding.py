08df9f8c7e33fba3d5feb0100f83af9491e5c99a
dhruvramani <dhruvramani98@gmail.com>
2017-09-29 01:33:30 +0530

    REQ.TXT

--- model/src/embedding.py
+++ model/src/embedding.py
@@ -12,4 +12,6 @@ class Embedding(object):
         gray_scale = np.asarray(gray_scale, dtype='uint8')
         faceCascade = cv2.CascadeClassifier(os.path.join(self.config.cascade_path , self.config.cascade))
         faces = faceCascade.detectMultiScale(gray_scale, scaleFactor=1.08, minNeighbors=1)
-        return gray_scale[faces[1]: faces[1] + faces[2], faces[0]: faces[0] + faces[3]]
+        if(str(type(faces)) == "<class 'tuple'>"):
+            return np.zeros((32, 32))
+        return gray_scale[faces[0, 1]: faces[0, 1] + faces[0, 2], faces[0, 0]: faces[0, 0] + faces[0, 3]].shape
